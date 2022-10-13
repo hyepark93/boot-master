9a4bc4f16400b91a61f5247c08527643fd40a5e6
Hyejeong Park <hyepark93@gmail.com>
2022-10-13 21:10:53 +0900

    springboot/aws 3장 JPA

---
+++ src/main/java/com/ttosik/start/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.ttosik.start.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
---
+++ src/main/java/com/ttosik/start/springboot/web/PostsApiController.java
@@ -0,0 +1,31 @@
+package com.ttosik.start.springboot.web;
+
+
+import com.ttosik.start.springboot.service.posts.PostsService;
+import com.ttosik.start.springboot.web.dto.PostsResponseDto;
+import com.ttosik.start.springboot.web.dto.PostsSaveRequestDto;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsSaveRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id){
+        return postsService.findById(id);
+    }
+}
